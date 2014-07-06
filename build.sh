62434cdd24cfca93027c80f92c514bb7d0ba30b2
Max Hollmann <mail@maxhollmann.de>
2014-07-06 15:36:35 +0200

    beginning of intro

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 
 rm -r build/*
-latexmk -p -pdf -jobname=build/Paper main.tex
+cat references.bib | \
+  grep -v "note = " | \
+  grep -v "urldate = " | \
+  grep -v "url = " | \
+  sed -e "s/_????/_nodate/" | \
+  sed -e "s/title = {Beta Oscillatory Activity Indexes Time Estimation},/title = {Beta Oscillatory Activity Indexes Time Estimation}, year = {in preparation},/" \
+  > clean_references.bib
+latexmk -pvc -pdf -jobname=build/Paper main.tex
