5169ec3cd58fff7a51480d5931c952d69e128073
Max Hollmann <mail@maxhollmann.de>
2014-07-07 16:31:30 +0200

    more intro, tables updated

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ cat references.bib | \
   grep -v "urldate = " | \
   grep -v "url = " | \
   sed -e "s/_????/_nodate/" | \
-  sed -e "s/title = {Beta Oscillatory Activity Indexes Time Estimation},/title = {Beta Oscillatory Activity Indexes Time Estimation}, year = {in preparation},/" \
+  sed -e "s/Manuscript in preparation/{Manuscript in preparation}/" \
   > clean_references.bib
 latexmk -pvc -pdf -jobname=build/Paper main.tex
+
+#  sed -e "s/title = {Beta Oscillatory Activity Indexes Time Estimation},/title = {Beta Oscillatory Activity Indexes Time Estimation}, year = {in preparation},/" \
